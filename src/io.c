b331029084243beea13e73ec8aea19fba9cf0e3a
calin <calin.nicolau@gmail.com>
2017-06-27 14:56:33 +0300

    Add inw.

--- src/io.c
+++ src/io.c
@@ -7,6 +7,13 @@ uint8_t inb(uint16_t port)
     return ret;
 }
 
+uint16_t inw(uint16_t port)
+{
+    uint16_t ret;
+    asm volatile("inw %%dx, %%ax" : "=a" (ret): "d" (port));
+    return ret;
+}
+
 void outb(uint16_t port, uint8_t value)
 {
    asm volatile ("outb %%al,%%dx": :"d" (port), "a" (value));
