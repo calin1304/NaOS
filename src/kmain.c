94c5adbb87cdb3ef365e302b75fc576deb2cef99
Gogs <gogs@fake.local>
2017-06-27 12:40:57 +0300

    Add beautiful messages.

--- src/kmain.c
+++ src/kmain.c
@@ -7,42 +7,47 @@
 #include "io.h"
 #include "pic.h"
 #include "clock.h"
+#include "malloc.h"
 
 extern Console console;
 extern Clock clock;
 
 void main(void) 
 {
-    // clock_init(&clock);    
-    // console_init(&console);
-    // console_printf(&console, "[#] Console initialized\n");
+    clock_init(&clock);    
+    console_init(&console);
+    console_printf(&console, "[#] Console initialized\n");
     
-    // console_printf(&console, "[#] Initializing GDT\n");
+    console_printf(&console, "[#] Initializing GDT\n");
     gdt_init();
     
-    // console_printf(&console, "[#] Initializing PICs and remapping IRQs to %x-%x\n", 0x20, 0x28+7);
+    console_printf(&console, "[#] Initializing PICs and remapping IRQs to %x-%x\n", 0x20, 0x28+7);
     init_pics(0x20, 0x28);
     outb(PIC1_DATA, 0xFC);
     
-    // console_printf(&console, "[#] Initializing IDT\n");
+    console_printf(&console, "[#] Initializing IDT\n");
     idt_init();
 
-    // console_printf(&console, "[%x] Kernel is running\n", clock.ticks);
+    console_printf(&console, "[%x] Kernel is running\n", clock.ticks);
 
+    heap_initialize();
+    int *a = (int*)malloc(sizeof(int) * 100);
+    int *b = (int*)malloc(sizeof(int));
+    console_printf(&console, "Addr: %p %p\n", a, b);
+    
+    // int n = 200;
+    // int m = 320;
+    // uint8_t *vga = (uint8_t*)0xA0000;
+    // for (int i = 0; i < n; ++i) {
+    //     for (int j = 0; j < m; ++j) {
+    //         vga[i*m + j] = 1;
+    //     }
+    // }
 
-    int n = 200;
-    int m = 320;
-    uint8_t *vga = (uint8_t*)0xA0000;
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            vga[i*m + j] = 1;
-        }
-    }
-
-    for (int i = n/3; i < ((n/3) << 1); ++i) {
-        for (int j = m/3; j < ((m/3) << 1); ++j) {
-            vga[i*m + j] = 2;
-        }
-    }
+    // for (int i = n/3; i < ((n/3) << 1); ++i) {
+    //     for (int j = m/3; j < ((m/3) << 1); ++j) {
+    //         vga[i*m + j] = 2;
+    //     }
+    // }
     for(;;);
 }
