b58f8927bb3ab9be22f0598e21da591f94f6a654
calin <calin.nicolau@gmail.com>
2017-07-15 08:40:40 +0300

    Add helper funtions.

---
+++ libk/ctype/tolower.c
@@ -0,0 +1,10 @@
+#include "libk/include/ctype.h"
+
+int tolower(int c)
+{
+    char ch = (char)c;
+    if (ch >= 'A' && ch <= 'Z') {
+        return (int)(ch - 'A' + 'a');
+    }
+    return c;
+}
---
+++ libk/include/ctype.h
@@ -0,0 +1,7 @@
+#ifndef TYPE_H
+#define TYPE_H
+
+int tolower(int c);
+int isalpha(int c);
+
+#endif
