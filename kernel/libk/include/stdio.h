2d30c674d89bef7b48e104191ea103325ec8e04a
Calin <calin.nicolau@gmail.com>
2017-11-21 16:10:15 +0200

    Add sprintf in libk

--- kernel/libk/include/stdio.h
+++ kernel/libk/include/stdio.h
@@ -8,6 +8,7 @@
 void putchar(int c);
 void puts(const char *s);
 void printf(const char *format, ...);
+void sprintf(char *s, const char *format, ...);
 FILE *fopen(const char *filename, const char *mode);
 size_t fread(void *ptr, size_t size, size_t count, FILE *f);
 
