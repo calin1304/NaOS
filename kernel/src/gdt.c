8c53b8509c181ab398171e0deb464caf0b671ee1
Calin Nicolau <calin.nicolau@yahoo.com>
2019-02-02 19:00:24 +0200

    replace gdt access and flag fields with bitfields

--- kernel/src/gdt.c
+++ kernel/src/gdt.c
@@ -7,7 +7,6 @@ enum GDT_ACCESS {
     GDT_RW              = 0x02,
     GDT_DC              = 0x04,
     GDT_EXECUTABLE      = 0x08,
-    GDT_ALWAYS_1        = 0x10,
     GDT_PRIVILEGE_1     = 0x20,
     GDT_PRIVILEGE_2     = 0x40,
     GDT_PRIVILEGE_3     = 0x60,
@@ -20,12 +19,26 @@ enum GDT_FLAGS {
 };
 
 struct gdt_entry {
-    uint16_t    limit;
-    uint32_t    base    : 24;
-    uint8_t     access;
-    uint8_t     limit2  : 4;
-    uint8_t     flags   : 4;
-    uint8_t     base3;
+    uint16_t limit0;
+    uint32_t base0 : 24;
+    uint8_t accessed : 1;
+    /* is read access for code segments allowed (write is never allowed)
+       is write access for data segments allowed (read is always allowed)*/
+    uint8_t rw : 1; 
+    /* data selector: 0 => segment grows up, 1 => grows down (offset > limit)
+       code selector: 1 => code can be executed from equal or lower privilege
+            (ring 3 far jump to conforming code in ring 2)
+    */
+    uint8_t dc : 1;
+    uint8_t executable : 1; // 0 => data selector
+    uint8_t s : 1; // descriptor type, 1 for code or data, 0 for system
+    uint8_t privilege : 2;
+    uint8_t present : 1;
+    uint8_t limit1 : 4;
+    uint8_t zero : 2;
+    uint8_t size : 1; // 0 for 16 bit pmode, 1 for 32 bit pmode
+    uint8_t granularity : 1; // 0 => limit is 1 B blocks, 1 => limit 4 KiB blocks
+    uint8_t base1;
 } __attribute__((packed));
 
 struct gdt_ptr {
@@ -47,12 +60,21 @@ static inline void gdt_load(struct gdt_ptr *ptr)
 static void gdt_entry_init(struct gdt_entry *entry, 
     uint32_t limit, uint32_t base, uint8_t access, uint8_t flags)
 {
-    entry->limit    = (uint16_t)(limit & 0xffff);
-    entry->limit2   = (limit & 0xf0000) >> 16;
-    entry->base     = (uint32_t)(base & 0xffffff);
-    entry->base3    = (uint8_t)((base & 0xff000000) >> 24);
-    entry->access   = access;
-    entry->flags    = flags;
+    memset(entry, 0, sizeof(gdt_entry_t));
+    entry->limit0 = (uint16_t)(limit & 0xffff);
+    entry->limit1 = (limit & 0xf0000) >> 16;
+    entry->base0 = (uint32_t)(base & 0xffffff);
+    entry->base1 = (uint8_t)((base & 0xff000000) >> 24);
+    // Set access bits
+    entry->rw = (access & GDT_RW) >> 1;
+    entry->dc = (access & GDT_DC) >> 2;
+    entry->executable = (access & GDT_EXECUTABLE) >> 3;
+    entry->privilege = (access & GDT_PRIVILEGE_3) >> 5;
+    entry->s = 1;
+    entry->present = 1;
+    // Set flag bits
+    entry->size = (flags & GDT_SIZE) >> 2;
+    entry->granularity = (flags & GDT_GRANULARITY) >> 3;
 }
 
 static inline void load_tsr(uint16_t sel)
@@ -99,11 +121,11 @@ void gdt_init()
     
     memset(&gdt_entries[0], 0, sizeof(struct gdt_entry));
 
-    gdt_entry_init(&gdt_entries[1], 0xffffffff, 0x0, GDT_ALWAYS_1 | GDT_PRESENT | GDT_EXECUTABLE | GDT_RW | GDT_DC, GDT_SIZE | GDT_GRANULARITY);
-    gdt_entry_init(&gdt_entries[2], 0xffffffff, 0x0, GDT_ALWAYS_1 | GDT_PRESENT | GDT_RW, GDT_SIZE | GDT_GRANULARITY);
+    gdt_entry_init(&gdt_entries[1], 0xffffffff, 0, GDT_EXECUTABLE | GDT_RW | GDT_DC, GDT_SIZE | GDT_GRANULARITY);
+    gdt_entry_init(&gdt_entries[2], 0xffffffff, 0, GDT_RW, GDT_SIZE | GDT_GRANULARITY);
     
-    gdt_entry_init(&gdt_entries[3], 0xffffffff, 0x0, GDT_ALWAYS_1 | GDT_PRESENT | GDT_EXECUTABLE | GDT_RW | GDT_PRIVILEGE_3 | GDT_DC, GDT_SIZE | GDT_GRANULARITY);
-    gdt_entry_init(&gdt_entries[4], 0xffffffff, 0x0, GDT_ALWAYS_1 | GDT_PRESENT | GDT_RW | GDT_PRIVILEGE_3, GDT_SIZE | GDT_GRANULARITY);
+    gdt_entry_init(&gdt_entries[3], 0xffffffff, 0, GDT_EXECUTABLE | GDT_RW | GDT_PRIVILEGE_3 | GDT_DC, GDT_SIZE | GDT_GRANULARITY);
+    gdt_entry_init(&gdt_entries[4], 0xffffffff, 0, GDT_RW | GDT_PRIVILEGE_3, GDT_SIZE | GDT_GRANULARITY);
 
     gdt_load(&gdt_ptr);
 }
