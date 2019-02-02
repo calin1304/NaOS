821a206eefefe728d91efd85de05089b7ba721ef
Calin Nicolau <calin.nicolau@yahoo.com>
2019-02-02 18:21:54 +0200

    Make gdt stuff private

--- kernel/include/gdt.h
+++ kernel/include/gdt.h
@@ -3,40 +3,6 @@
 
 #include <stdint.h>
 
-enum GDT_ACCESS {
-    GDT_ACCESSED        = 0x01,
-    GDT_RW              = 0x02,
-    GDT_DC              = 0x04,
-    GDT_EXECUTABLE      = 0x08,
-    GDT_ALWAYS_1        = 0x10,
-    GDT_PRIVILEGE_1     = 0x20,
-    GDT_PRIVILEGE_2     = 0x40,
-    GDT_PRIVILEGE_3     = 0x60,
-    GDT_PRESENT         = 0x80
-};
-
-enum GDT_FLAGS {
-    GDT_SIZE        = 0x4,
-    GDT_GRANULARITY = 0x8
-};
-
-struct GDTEntry {
-    uint16_t    limit;
-    uint32_t    base    : 24;
-    uint8_t     access;
-    uint8_t     limit2  : 4;
-    uint8_t     flags   : 4;
-    uint8_t     base3;
-} __attribute__((packed));
-
-struct GDTPtr {
-    uint16_t limit;
-    uint32_t base;
-} __attribute__((packed));
-
-struct GDTEntry gdt_entries[6];
-struct GDTPtr   gdt_ptr;
-
 void gdt_init();
 void install_tss(uint16_t kernelSS, uint32_t kernelESP);
 
--- kernel/src/gdt.c
+++ kernel/src/gdt.c
@@ -2,6 +2,40 @@
 
 #include <libk/string.h>
 
+enum GDT_ACCESS {
+    GDT_ACCESSED        = 0x01,
+    GDT_RW              = 0x02,
+    GDT_DC              = 0x04,
+    GDT_EXECUTABLE      = 0x08,
+    GDT_ALWAYS_1        = 0x10,
+    GDT_PRIVILEGE_1     = 0x20,
+    GDT_PRIVILEGE_2     = 0x40,
+    GDT_PRIVILEGE_3     = 0x60,
+    GDT_PRESENT         = 0x80
+};
+
+enum GDT_FLAGS {
+    GDT_SIZE        = 0x4,
+    GDT_GRANULARITY = 0x8
+};
+
+struct GDTEntry {
+    uint16_t    limit;
+    uint32_t    base    : 24;
+    uint8_t     access;
+    uint8_t     limit2  : 4;
+    uint8_t     flags   : 4;
+    uint8_t     base3;
+} __attribute__((packed));
+
+struct GDTPtr {
+    uint16_t limit;
+    uint32_t base;
+} __attribute__((packed));
+
+struct GDTEntry gdt_entries[6];
+struct GDTPtr gdt_ptr;
+
 static void gdt_load(struct GDTPtr *ptr)
 {
 	__asm__ __volatile__("lgdt (%0)" : : "r"(ptr));
@@ -79,7 +113,7 @@ void install_tss(uint16_t kernelSS, uint32_t kernelESP)
     memset((void*)&TSS, 0, sizeof(struct tss_entry));
     
     TSS.ss0 = kernelSS;
-    TSS.esp0 = kernelESP;
+    TSS.esp0 = kernelESP;   
     TSS.cs = 0x0b;
     TSS.ss = 0x13;
 	TSS.es = 0x13;
